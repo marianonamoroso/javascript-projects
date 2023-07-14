e5f8e2a2bdd93e4f5b7861595090b7ff4374ea8b
Mariano N Amoroso <marianonamoroso@Marianos-MBP.fibertel.com.ar>
2023-07-14 17:02:17 -0300

    DOM: 02 Challenge

---
+++ learning/challenges/dom/02/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Variables
+const btnShowModal = document.querySelectorAll('.show-modal')
+const modal = document.querySelector('.modal') 
+const btnCloseModal = document.querySelector('.close-modal') 
+const overlay = document.querySelector('.overlay')
+
+// Functions
+const closeModal = function(){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+const showModal = function(){
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+// Show Modal
+for (let index = 0; index < btnShowModal.length; index++) {
+    btnShowModal[index].addEventListener('click', showModal)
+}
+
+// Close Modal
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
