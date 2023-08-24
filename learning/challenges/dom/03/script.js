18be0ebc2d0b3a9c90da015d02a08842b3b47845
Mariano N Amoroso <marianonamoroso@Marianos-MBP.fibertel.com.ar>
2023-08-24 09:58:16 -0300

    DOM: New exercises - WIP

--- learning/challenges/dom/03/script.js
+++ learning/challenges/dom/03/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+/* 
+ID1 = document.querySelector('#<id>') 
+ID2 = document.getElementById('<id>')
+
+Class = document.querySelector('.<class-name>')
+*/
+
+// Selecting elements
+const score0El = document.querySelector('#score--0') 
+const score1El = document.getElementById('score--1')
+const diceEl = document.querySelector('.dice') 
+
+score0El.textContent = 0
+score1El.textContent = 0
+diceEl.classList.add('hidden')
+
